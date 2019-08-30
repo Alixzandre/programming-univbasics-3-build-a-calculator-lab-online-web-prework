cd31dc33544f2eeaeeb1a03b308f85805715aab2
Alexander Bispo <alexbispo173@gmail.com>
2019-08-30 03:07:13 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,18 +9,21 @@ return diference
 end
 
 def division(num1, num2)
-product = num1*num2
+qoutient = num2/num1
 return product
 end
 
 def multiplication(num1, num2)
-
+product = num1*num2
+return product
 end
 
 def modulo(num1, num2)
-
+m = num2%num1
+return m
 end
 
 def square_root(num)
-
+sR=sqrt(num)
+return sR
 end
