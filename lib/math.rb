aaeca64e36f488bf231b1dfdeee3e63b1cde2360
Alexander Bispo <alexbispo173@gmail.com>
2019-08-30 03:05:13 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,16 @@
 def addition(num1, num2)
-
+sum = num1+num2
+return sum
 end
 
 def subtraction(num1, num2)
-
+diference =num2-num1
+return diference
 end
 
 def division(num1, num2)
-
+product = num1*num2
+return product
 end
 
 def multiplication(num1, num2)
